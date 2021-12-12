273bf6e99cb45f44376230a209a59a1997f7461f
tfavro <wetr9902@gmail.com>
2021-12-12 16:44:05 -0600

    delete functionality works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -99,4 +100,23 @@ return actionContainerEL;
 
 formEl.addEventListener("submit", taskFormHandler);
 console.dir()
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id ='" + taskId + "']");
+    taskSelected.remove();
+
+};
+
+var taskButtonHandler = function(event){
+    console.log(event.target);
+
+    if(event.target.matches(".delete-btn")){
+        //get the elements task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 
