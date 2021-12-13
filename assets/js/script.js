bdced498e52df84574042522f52722ae2149e84a
tfavro <wetr9902@gmail.com>
2021-12-12 19:36:29 -0600

    added edit functionality to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,25 +98,46 @@ actionContainerEL.appendChild(statusSelectEl);
 return actionContainerEL;
 };
 
-formEl.addEventListener("submit", taskFormHandler);
-console.dir()
-
 var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id ='" + taskId + "']");
     taskSelected.remove();
+};
 
+var editTask = function(taskId){
+    //get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    //get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    document.querySelector("input[name='task-name']").value = taskName;
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    document.querySelector("select[name='task-type']").value = taskType;
+
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
+    
 };
 
 var taskButtonHandler = function(event){
-    console.log(event.target);
+   //get target element from event
+   var targetEl = event.target;
 
-    if(event.target.matches(".delete-btn")){
+   //edit button was clicked
+   if (targetEl.matches(".edit-btn")){
+       var taskId = targetEl.getAttribute("data-task-id");
+       editTask(taskId);
+   }
+   //delete button was clicked
+    else if(event.target.matches(".delete-btn")){
         //get the elements task id
         var taskId = event.target.getAttribute("data-task-id");
         deleteTask(taskId);
     }
 };
 
-
+formEl.addEventListener("submit", taskFormHandler);
+console.dir()
 pageContentEl.addEventListener("click", taskButtonHandler);
 
